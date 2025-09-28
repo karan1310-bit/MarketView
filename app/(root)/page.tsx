219ebb21fd7f5f5ba183937215bb0eafe4607794
karan <karanbhati1310@gmail.com>
2025-09-28 16:48:33 +0530

    auth UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,7 +11,7 @@ const Home = () => {
     const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
 
     return (
-      <main>
+      <main className="font-DMregular">
         <div className="flex flex-col items-center justify-center text-center px-4 py-2 mb-12 md:mb-16 md:py-4 gap-7">
         <div className="max-w-4xl text-center  space-y-6 md:space-y-6 relative">
           <h1 className="text-4xl md:text-7xl font-DMbold text-gray-500 leading-none">
